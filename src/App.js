04ade1d72bcb1713f60a4ce53ddb1d3279a02948
Sheikh_Ahmed <ssahme21@gmail.com>
2022-09-26 22:37:20 -0400

    Added API call, fixed env issue, added searchbar

--- src/App.js
+++ src/App.js
@@ -1,96 +1,36 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useEffect } from "react";
 import "./App.css";
-import MovieList from "./Components/MovieList";
+import MovieList from "./Components/MovieList/MovieList";
+import MovieListHeading from "./Components/MovieListHeading/MovieListHeading";
+import Searchbox from "./Components/Searchbox/Searchbox";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 function App() {
   // const [movies, setMovies] = useState([]);
 
-  const [movies, setMovies] = useState([
-    {
-      Title: "The Avengers",
-      Year: "2012",
-      imdbID: "tt0848228",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-    },
-    {
-      Title: "Avengers: Endgame",
-      Year: "2019",
-      imdbID: "tt4154796",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
-    },
-    {
-      Title: "Avengers: Infinity War",
-      Year: "2018",
-      imdbID: "tt4154756",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
-    },
-    {
-      Title: "Avengers: Age of Ultron",
-      Year: "2015",
-      imdbID: "tt2395427",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg",
-    },
-    {
-      Title: "The Avengers",
-      Year: "1998",
-      imdbID: "tt0118661",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BYWE1NTdjOWQtYTQ2Ny00Nzc5LWExYzMtNmRlOThmOTE2N2I4XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg",
-    },
-    {
-      Title: "The Avengers: Earth's Mightiest Heroes",
-      Year: "2010–2012",
-      imdbID: "tt1626038",
-      Type: "series",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BYzA4ZjVhYzctZmI0NC00ZmIxLWFmYTgtOGIxMDYxODhmMGQ2XkEyXkFqcGdeQXVyNjExODE1MDc@._V1_SX300.jpg",
-    },
-    {
-      Title: "Ultimate Avengers: The Movie",
-      Year: "2006",
-      imdbID: "tt0491703",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMTYyMjk0NTMwMl5BMl5BanBnXkFtZTgwNzY0NjAwNzE@._V1_SX300.jpg",
-    },
-    {
-      Title: "Ultimate Avengers II",
-      Year: "2006",
-      imdbID: "tt0803093",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BZjI3MTI5ZTYtZmNmNy00OGZmLTlhNWMtNjZiYmYzNDhlOGRkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-    },
-    {
-      Title: "The Avengers",
-      Year: "1961–1969",
-      imdbID: "tt0054518",
-      Type: "series",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BZWQwZTdjMDUtNTY1YS00MDI0LWFkNjYtZDA4MDdmZjdlMDRlXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg",
-    },
-    {
-      Title: "Avengers Assemble",
-      Year: "2012–2019",
-      imdbID: "tt2455546",
-      Type: "series",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMTY0NTUyMDQwOV5BMl5BanBnXkFtZTgwNjAwMTA0MDE@._V1_SX300.jpg",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const getMovieRequest = async (searchTerm) => {
+    const url = `http://www.omdbapi.com/?s=${searchTerm}&apikey=${API_KEY}`;
+    const response = await fetch(url).then((response) => response.json());
+    if (response.Search) {
+      setMovies(response.Search);
+    }
+  };
+
+  useEffect(() => {
+    getMovieRequest(searchTerm);
+  }, [searchTerm]);
 
   return (
     <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Movies" />
+        <Searchbox searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
       <div className="row">
         <MovieList movies={movies} />
       </div>
