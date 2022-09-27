92bf744de2405a32dbde9a5dea253ba6da243bf2
Sheikh_Ahmed <ssahme21@gmail.com>
2022-09-27 12:10:03 -0400

    Added favorites functionality, need to add duplicate check

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import "./App.css";
 import MovieList from "./Components/MovieList/MovieList";
 import MovieListHeading from "./Components/MovieListHeading/MovieListHeading";
 import Searchbox from "./Components/Searchbox/Searchbox";
+import AddFavorites from "./Components/AddFavorites/AddFavorites";
+import RemoveFavorites from "./Components/RemoveFavorites/RemoveFavorites";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 function App() {
-  // const [movies, setMovies] = useState([]);
-
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [favorites, setFavorites] = useState([]);
 
   const getMovieRequest = async (searchTerm) => {
     const url = `http://www.omdbapi.com/?s=${searchTerm}&apikey=${API_KEY}`;
@@ -21,9 +22,30 @@ function App() {
     }
   };
 
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem(
+      "movie-list-application-favorites",
+      JSON.stringify(items)
+    );
+  };
+
   useEffect(() => {
     getMovieRequest(searchTerm);
   }, [searchTerm]);
+
+  const AddFavoriteMovie = (movie) => {
+    const newFavoriteList = [...favorites, movie];
+    setFavorites(newFavoriteList);
+    saveToLocalStorage(newFavoriteList);
+  };
+
+  const removeFavoriteMovie = (movie) => {
+    const newFavoriteList = favorites.filter(
+      (favorite) => favorite.imdbID !== movie.imdbID
+    );
+    setFavorites(newFavoriteList);
+    saveToLocalStorage(newFavoriteList);
+  };
 
   return (
     <div className="container-fluid movie-app">
@@ -32,7 +54,21 @@ function App() {
         <Searchbox searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
       <div className="row">
-        <MovieList movies={movies} />
+        <MovieList
+          movies={movies}
+          handleFavorites={AddFavoriteMovie}
+          favoriteComponent={AddFavorites}
+        />
+      </div>
+      <div className="row d-flex align-items center mt-4 mb-4">
+        <MovieListHeading heading="Favorites" />
+      </div>
+      <div className="row">
+        <MovieList
+          movies={favorites}
+          handleFavorites={removeFavoriteMovie}
+          favoriteComponent={RemoveFavorites}
+        />
       </div>
     </div>
   );
--- src/Components/MovieList/MovieList.js
+++ src/Components/MovieList/MovieList.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-const MovieList = ({ movies }) => {
+const MovieList = ({ movies, favoriteComponent, handleFavorites }) => {
+  const FavoriteComponent = favoriteComponent;
   return (
-    <div>
+    <div className="container">
       {movies.map((movie, index) => (
-        <div className="image-container d-flex justify-content-start m3" key={movie.imdbID}>
+        <div
+          className="image-container d-flex justify-content-start m3"
+          key={movie.imdbID}
+        >
           <img src={movie.Poster} alt="movie poster" />
+          <div
+            onClick={() => handleFavorites(movie)}
+            className="overlay d-flex align-items-center justify-content-center"
+          >
+            <FavoriteComponent/>
+          </div>
         </div>
       ))}
     </div>
